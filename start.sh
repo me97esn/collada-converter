a05ce3493c2ccd37102f52b9c848ccb0c7eb3d5c
Emil Stenberg <esandin@gmail.com>
2016-05-04 16:26:54 +0200

    refactor some code into html file

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-browser-sync start --server --startPath examples/convert.html --files **/*.* --port 3333
+browser-sync start --server --startPath examples/load-model.html --files **/*.* --port 3333
